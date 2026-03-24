cda310cd8d36540c39e5a054f5a44d30faa117b3
Jayanta Mukherjee <jmukher@newton.cs.purdue.edu>
2026-03-24 01:38:23 -0400

    script update

--- drivers/model_static_vary_p.sh
+++ drivers/model_static_vary_p.sh
@@ -19,21 +19,23 @@ RECENCY_PROBABILITIES="sj_recprob"
 
 SAME_YEAR_PROPORTION="0.12"
 FULLY_RANDOM_CITATIONS="0.05"
-ER_PROBABILITY="0.001"
 LOG_LEVEL="1"
 MODEL="er-gnp"
+ER_PROBABILITY="0.01"
 
-for GROWTH_PERCENT in 1 3 5 6
+for GROWTH_PERCENT in 6
 do
     GROWTH_RATE=$(echo "scale=4; $GROWTH_PERCENT/100" | bc -l)
     GROWTH_LABEL=$(echo "$GROWTH_PERCENT" | sed 's/0\./dot/; s/\./dot/') 
 
     ER_PROBABILITY_LABEL=$(awk "BEGIN {printf \"%.1f\", 100 * $ER_PROBABILITY}")
 
-    OUTPUT_FILE="./output/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p.edgelist"
-    OUTPUT_LOG="./output/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p-${NUM_THREADS}t.log"
-    OUTPUT_AUX="./output/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p.aux"
-    OUT_FILE="./output/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p-${NUM_THREADS}t.out"
+    mkdir -p output/${ER_PROBABILITY_LABEL}
+
+    OUTPUT_FILE="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p.edgelist"
+    OUTPUT_LOG="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p-${NUM_THREADS}t.log"
+    OUTPUT_AUX="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p.aux"
+    OUT_FILE="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p-${NUM_THREADS}t.out"
 
     echo "Running growth rate = ${GROWTH_RATE} = ${GROWTH_PERCENT}% with ${NUM_THREADS} thread"
     echo $OUTPUT_FILE
@@ -63,3 +65,94 @@ do
     1>${OUT_FILE}
 
 done
+
+ER_PROBABILITY="0.03"
+
+for GROWTH_PERCENT in 1 3 6
+do
+    GROWTH_RATE=$(echo "scale=4; $GROWTH_PERCENT/100" | bc -l)
+    GROWTH_LABEL=$(echo "$GROWTH_PERCENT" | sed 's/0\./dot/; s/\./dot/') 
+
+    ER_PROBABILITY_LABEL=$(awk "BEGIN {printf \"%.1f\", 100 * $ER_PROBABILITY}")
+
+    mkdir -p output/${ER_PROBABILITY_LABEL}
+
+    OUTPUT_FILE="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p.edgelist"
+    OUTPUT_LOG="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p-${NUM_THREADS}t.log"
+    OUTPUT_AUX="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p.aux"
+    OUT_FILE="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p-${NUM_THREADS}t.out"
+
+    echo "Running growth rate = ${GROWTH_RATE} = ${GROWTH_PERCENT}% with ${NUM_THREADS} thread"
+    echo $OUTPUT_FILE
+    echo $OUTPUT_AUX
+
+    time ./abm \
+    --model er-gnp \
+    --er-probability ${ER_PROBABILITY} \
+    --edgelist ${INPUT_EDGELIST} \
+    --nodelist ${INPUT_NODELIST} \
+    --out-degree-bag ${OUTDEGREE_BAG} \
+    --recency-probabilities ${RECENCY_PROBABILITIES} \
+    --alpha 0.5 \
+    --preferential-weight 0.33 \
+    --recency-weight 0.33 \
+    --fitness-weight 0.33 \
+    --growth-rate ${GROWTH_RATE} \
+    --fully-random-citations ${FULLY_RANDOM_CITATIONS} \
+    --num-cycles ${NUM_CYCLES} \
+    --same-year-proportion ${SAME_YEAR_PROPORTION} \
+    --output-file ${OUTPUT_FILE} \
+    --auxiliary-information-file ${OUTPUT_AUX} \
+    --log-file ${OUTPUT_LOG} \
+    --num-processors ${NUM_THREADS} \
+    --log-level ${LOG_LEVEL} \
+    2>${ERRORS}/abm-${GROWTH_PERCENT}p.err \
+    1>${OUT_FILE}
+
+done
+
+ER_PROBABILITY="0.05"
+
+for GROWTH_PERCENT in 1 3 6
+do
+    GROWTH_RATE=$(echo "scale=4; $GROWTH_PERCENT/100" | bc -l)
+    GROWTH_LABEL=$(echo "$GROWTH_PERCENT" | sed 's/0\./dot/; s/\./dot/')
+
+    ER_PROBABILITY_LABEL=$(awk "BEGIN {printf \"%.1f\", 100 * $ER_PROBABILITY}")
+
+    mkdir -p output/${ER_PROBABILITY_LABEL}
+
+    OUTPUT_FILE="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p.edgelist"
+    OUTPUT_LOG="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p-${NUM_THREADS}t.log"
+    OUTPUT_AUX="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p.aux"
+    OUT_FILE="./output/${ER_PROBABILITY_LABEL}/cpu-static-model-${MODEL}-${NUM_CYCLES}y-${GROWTH_LABEL}p-${ER_PROBABILITY_LABEL}p-${NUM_THREADS}t.out"
+
+    echo "Running growth rate = ${GROWTH_RATE} = ${GROWTH_PERCENT}% with ${NUM_THREADS} thread"
+    echo $OUTPUT_FILE
+    echo $OUTPUT_AUX
+
+    time ./abm \
+    --model er-gnp \
+    --er-probability ${ER_PROBABILITY} \
+    --edgelist ${INPUT_EDGELIST} \
+    --nodelist ${INPUT_NODELIST} \
+    --out-degree-bag ${OUTDEGREE_BAG} \
+    --recency-probabilities ${RECENCY_PROBABILITIES} \
+    --alpha 0.5 \
+    --preferential-weight 0.33 \
+    --recency-weight 0.33 \
+    --fitness-weight 0.33 \
+    --growth-rate ${GROWTH_RATE} \
+    --fully-random-citations ${FULLY_RANDOM_CITATIONS} \
+    --num-cycles ${NUM_CYCLES} \
+    --same-year-proportion ${SAME_YEAR_PROPORTION} \
+    --output-file ${OUTPUT_FILE} \
+    --auxiliary-information-file ${OUTPUT_AUX} \
+    --log-file ${OUTPUT_LOG} \
+    --num-processors ${NUM_THREADS} \
+    --log-level ${LOG_LEVEL} \
+    2>${ERRORS}/abm-${GROWTH_PERCENT}p.err \
+    1>${OUT_FILE}
+
+done
+ 
